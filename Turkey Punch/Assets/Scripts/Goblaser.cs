1718de1f6a328cc1034a74054d6e666d5d270280
Ethan Nakashima <ethanmn@hotmail.com>
2015-11-22 11:47:53 -0800

    Starting Narwhal

--- Turkey Punch/Assets/Scripts/Goblaser.cs
+++ Turkey Punch/Assets/Scripts/Goblaser.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class Goblaser : MonoBehaviour {
 
     float tickTimer, lifeTimer, life, tick;
-    int damage;
-
+    int damage, tickCount;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +12,8 @@ public class Goblaser : MonoBehaviour {
         lifeTimer = 0;
         life = 1.2f;
         tick = 0.02f;
-        damage = 5;
+        damage = 2;
+        tickCount = 0;
 	}
 	
 	// Update is called once per frame
@@ -21,11 +21,16 @@ public class Goblaser : MonoBehaviour {
         // Tick
 	    if (tickTimer >= tick)
         {
+            tickCount++;
             tickTimer = 0;
             GameObject p1 = GameObject.FindGameObjectWithTag("Santa");
             if (GetComponent<BoxCollider2D>().bounds.Contains(p1.transform.position))
             {
-                p1.GetComponent<ActorController>().SetState(new Santa_HitState(damage));
+                if (tickCount < 12)
+                    damage = 7;
+                else
+                    damage = 3;
+                    p1.GetComponent<ActorController>().SetState(new Santa_HitState(damage));
             }
         }
         else
