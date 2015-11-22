bfecc3c7755f664ac40262bdbc35f9541b93839c
Twitch572 <dude.dodiecat.dudet@gmail.com>
2015-11-22 02:37:30 -0800

    FUCK SHIT FUCK IT'S FUKCING FUCKSDAFKJFASDLKNFVANKM;DFKNMLDASJLBN

---
+++ Turkey Punch/Assets/Scripts/RandomizeMusicScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeMusicScript : MonoBehaviour {
+
+    AudioSource aSource;
+
+	// Use this for initialization
+	void Start () {
+        aSource = gameObject.GetComponent<AudioSource>();
+        float temp = Random.value;
+        if (temp < .5f)
+            aSource.clip = AudioClip.Create("Assets/Resources/Music/SantaTheme.mp3", 10, 10, 10, false);
+        else
+            aSource.clip = AudioClip.Create("Assets/Resources/Music/TurkeyTheme.mp3", 10, 10, 10, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
